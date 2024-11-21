521029a4102e5b5f914aff5b30900c6580369eaf
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-11-21 14:13:29 -0300

    Implementing the timer

--- src/pomojulia.jl
+++ src/pomojulia.jl
@@ -1,50 +1,65 @@
 # ==============================================
 # pomojulia
 # Author @wiirios
+# See more about pomodoro technique here 
+# https://en.wikipedia.org/wiki/Pomodoro_Technique
 # ==============================================
 
 using Alert
 using Dates
 
-const SHORTBREAK = 5
-const LONGBREAK = 15
-const POMODORO = 25
-
-defaultTimer = SHORTBREAK
-
 today = Dates.today()
 hourNow = hour(now())
 minutesNow = minute(now())
 
-struct teste
-    time::Int64
-    #sound::Bool
-end
+    mutable struct TimerStruct
+        minutes::Int64
+        seconds::Int64
+    end
 
-function createTimer(x::Int64)
-    return teste(x)
-end
+    SHORTBREAK = TimerStruct(5, 60)
+    LONGBREAK = TimerStruct(15, 60)
+    POMODORO = TimerStruct(25, 60)
 
-function getTimerName()
-    timeNameMap = Dict(SHORTBREAK => "Short Break", LONGBREAK => "Long Break")
-    return get(timeNameMap, defaultTimer, "Pomodoro")
-end
+    defaultTimer = LONGBREAK
 
-function getTimer()
-    timerMap = Dict(5 => SHORTBREAK, 15 => LONGBREAK)
-    return get(timerMap, defaultTimer, 25)
-end
+    function getTimerName()
+        timeNameMap = Dict(SHORTBREAK => "Short Break", LONGBREAK => "Long Break")
+        return get(timeNameMap, defaultTimer, "Pomodoro")
+    end
 
-#alert()
-println("""
-========================== PomoJulia ==========================
-                             $(hourNow):$(minutesNow)                
-1 - Start Timer
-2 - Set Break
-3 - Exit
+    function getTimer()
+        timerMap = Dict(SHORTBREAK => 5, LONGBREAK => 15)
+        return get(timerMap, defaultTimer, 25)
+    end
 
-* Timer at $(getTimer()) $(getTimerName())
-=======================================================
-""")
+    println("""
+    ========================== PomoJulia ==========================
+                                $(hourNow):$(minutesNow)                
+    1 - Start Timer
+    2 - Set Break
+    3 - Exit
 
-#print("\rUpdating... Step ", i, " of 10")
+    * Timer at $(getTimer()) $(getTimerName())
+    =======================================================
+    """)
+
+    function countdown(pomodoroOption::TimerStruct)
+        seconds = pomodoroOption.seconds
+        minutes = pomodoroOption.minutes
+        
+        while minutes > 0
+            for i in seconds:-1:0
+                print("\r",minutes, ":", seconds)
+                sleep(1)
+                seconds = seconds - 1
+
+                if seconds == 0 && minutes > 0
+                    seconds = 5
+                    minutes -= 1
+                end
+            end
+        end
+    end
+
+    #countdown(defaultTimer)
