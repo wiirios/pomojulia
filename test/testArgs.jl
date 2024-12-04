152ea2ddabe0914b77d1c31fb1883327cedd61dd
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-12-04 09:41:26 -0300

    style: remove comments

--- test/testArgs.jl
+++ test/testArgs.jl
@@ -1,10 +1,5 @@
 include("../src/pomojulia.jl")
 
-# -k 15
-# -p 25
-# -s 5
-
-# dictionary containing times 
 dict = IdDict("-k" => () -> countdown(LONGBREAK), "-p" => () -> countdown(POMODORO), "-s" => () -> countdown(SHORTBREAK))
 
 fun = get(dict, ARGS, () -> countdown(defaultTimer))
