54af1350dc9e4b2637f6c79f0531d3ecd852421b
Winnd11 <recuperacaowilliamrios@gmail.com>
2024-12-08 09:53:49 -0300

    feat: add command-line argument support in main

--- main.jl
+++ main.jl
@@ -1,3 +1,5 @@
 include("src/pomojulia.jl")
 
-start()
+dict = Dict("-k" => () -> countdown(LONGBREAK), "-p" => () -> countdown(POMODORO), "-s" => () -> countdown(SHORTBREAK))
+
+isempty(ARGS) ? start() : (fun = get(dict, ARGS[1], () -> countdown(defaultTimer)); fun())
